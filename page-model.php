412954e1e974cc6514d423a2d13a4a718202b45f
lucasweiby <lucas_weiby@hotmail.com>
2019-08-22 11:21:01 -0300

    Footer and exhibitor creation; Numbers, Home,  and Slider  modification

---
+++ page-model.php
@@ -0,0 +1,16 @@
+<?php include 'header.php' ?>
+
+<header class="header">
+    <?php include 'particles/nav.php'; ?>
+</header>
+
+<div class="home-interna">
+    <div class="overlay"></div>
+    <h1 class="home-interna-name">Contato</h1>
+</div>
+
+<section class="contact">
+    
+</section>
+
+<?php include 'footer.php'; ?>
